58c8de8c46fca37704b400bcc0c6925fdda181d2
Jaime Hernandez <jaimevazz@gmail.com>
2020-09-28 11:17:57 -0500

    Added pause menu in Level (gameplay)

---
+++ Assets/_Scripts/App/Level/Game/PauseManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public bool isPaused;
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    void Start()
+    {
+        // TODO: CHECK
+        ResumeGame();
+    }
+}
