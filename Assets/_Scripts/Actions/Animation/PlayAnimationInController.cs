d9415e60aa80f153e35f99056306dbe780a18d4a
Jaime Hernandez <jaimevazz@gmail.com>
2021-02-09 21:44:39 -0600

    Animation Actions

---
+++ Assets/_Scripts/Actions/Animation/PlayAnimationInController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayAnimationInController : MonoBehaviour 
+{
+	private Animator animator;
+
+	void Start ()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	public void Play(string name)
+	{
+		animator.Play(name, -1, 0f);
+	}
+
+	public void Stop()
+	{
+		animator.StopPlayback();
+	}
+}
