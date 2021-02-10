39ae48ba6997e47248a426a4df3a164c456c1efa
Jaime Hernandez <jaimevazz@gmail.com>
2021-02-09 18:40:28 -0600

    Added Unity Events for common types

---
+++ Assets/_Scripts/Events/UnityEvents.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class RaycastHitEvent : UnityEvent<RaycastHit> {}
+[Serializable]
+public class CollisionEvent : UnityEvent<Collision> {}
+[Serializable]
+public class ColliderEvent : UnityEvent<Collider> {}
+[Serializable]
+public class Vector3Event : UnityEvent<Vector3> {}
+[Serializable]
+public class StringEvent : UnityEvent<string> {}
+[Serializable]
+public class FloatEvent : UnityEvent<float> {}
+[Serializable]
+public class BoolEvent : UnityEvent<bool> {}
