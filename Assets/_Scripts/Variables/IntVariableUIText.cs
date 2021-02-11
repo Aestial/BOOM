e670e1bca5ac539d4edf4a38efa8a7198ea9af48
Jaime Hernandez <jaimevazz@gmail.com>
2021-02-10 23:18:31 -0600

    Added Variable Scripts and moved out Data

---
+++ Assets/_Scripts/Variables/IntVariableUIText.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class IntVariableUIText : MonoBehaviour 
+{
+	[SerializeField] IntVariable intVar = default;
+	[SerializeField] Text text = default;
+	[SerializeField] string format = "D2";
+
+	private int value;
+
+	void Start () 
+	{
+		UpdateUI();
+	}
+	
+	void Update () 
+	{
+		if (intVar.RuntimeValue != value)
+			UpdateUI();
+	}
+
+	void UpdateUI ()
+	{
+		value = intVar.RuntimeValue;
+		text.text = value.ToString(format);
+	}
+}
