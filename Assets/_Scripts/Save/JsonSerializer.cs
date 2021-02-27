1addf8400730df8986143e5a1c97717b55425d58
Jaime Hernandez <jaimevazz@gmail.com>
2021-02-27 00:23:59 -0600

    Fixed JsonSerializer

--- Assets/_Scripts/Save/JsonSerializer.cs
+++ Assets/_Scripts/Save/JsonSerializer.cs
@@ -2,34 +2,48 @@
 using System.IO;
 using UnityEngine;
 
-public abstract class JsonSerializer<T> : MonoBehaviour where T : MonoBehaviour
+public abstract class JsonSerializer<T> : ScriptableObject where T : ScriptableObject
 {    
-    [SerializeField] protected string fileName;
     string filePath;
-    protected void Awake()
+    virtual protected void Awake()
     {
-        filePath = Path.Combine(Application.persistentDataPath, fileName);
-        Debug.Log(filePath);
+
     }
     protected void SetFilePath(string fileName)
     {
-        this.fileName = fileName;
         filePath = Path.Combine(Application.persistentDataPath, fileName);
-        Debug.Log(filePath);
+        Debug.Log("File path: " + filePath);
+    }
+    protected void SetEmptyFilePath()
+    {
+        if (String.IsNullOrEmpty(filePath))
+            SetFilePath(name+".json");
     }
     protected void Save(object obj)
     {
+        SetEmptyFilePath();
         string json = JsonUtility.ToJson(obj);
         File.WriteAllText(filePath, json);
-        Debug.Log(json);
+        Debug.Log("Json saved into file: " + json);
     }
+    
     protected U GetFromFileOrCreate<U>(Func<U> createFunction)
     {        
-        return File.Exists(filePath) ? Load<U>() : createFunction();
+        SetEmptyFilePath();
+        if(File.Exists(filePath))
+        {
+            Debug.Log("File exists: " + filePath);  
+            return Load<U>();
+        }   
+        Debug.Log("File doesn't exist: " + filePath);  
+        return createFunction();
     }
+
     protected U Load<U>()
     {
+        Debug.Log( typeof(U).ToString() + " is loading from file...");
         string json = File.ReadAllText(filePath);
+        Debug.Log("Json loaded. Value: " + json);
         return JsonUtility.FromJson<U>(json);
     }
 }
